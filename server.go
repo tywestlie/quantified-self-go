95a3ee948009b14f6e5e2f3c235def73f115757a
Tyler Westlie <tyler.westlie@gmail.com>
2018-08-21 21:55:57 -0600

    root path responding with helloworld

--- server.go
+++ server.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+  // "encoding/json"
+  "log"
+  "net/http"
   "fmt"
+  // "math/rand"
+  // "strconv"
+  "github.com/gorilla/mux"
+
 )
 
+// Food Struct (Model)
+
+type Food struct {
+  ID  string `json :"id"`
+  Name  string `json :"name"`
+  calories  int `json :"calories"`
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Quantifed Self Go Backend")
+}
+
 func main() {
-  fmt.Println("Hello World!")
+  router := mux.NewRouter()
+
+  router.HandleFunc("/", root).Methods("GET")
+
+  log.Fatal(http.ListenAndServe(":3000", router))
 }
